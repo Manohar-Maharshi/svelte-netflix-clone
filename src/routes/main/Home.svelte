634ef0c9f29d68946c78f76663fe13c5e6610dcc
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-14 17:45:30 +0530

    Update Home.svelte

--- src/routes/main/Home.svelte
+++ src/routes/main/Home.svelte
@@ -5,4 +5,5 @@
 
 
 <main style="background-image: linear-gradient(to top, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%),url('https://image.tmdb.org/t/p/original/4vCh8R4yd6ybOmbxRAPOzaXmLTV.jpg'); background-repeat: no-repeat; background-size: cover;background-position: top;" class="bg-[#000cc] w-full h-[35rem]">
+	<h1>Hello</h1>
 </main>

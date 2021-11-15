11a0fd22fd086d6a62d69612f05103e487c461de
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-15 21:42:59 +0530

    🎈 TV Show's route - only show's details about tv shows categorized based on genres, popularity

--- src/routes/main/TvShows.svelte
+++ src/routes/main/TvShows.svelte
@@ -1,1 +1,28 @@
-<p>Tv</p>
+<script>
+	import TvRow from '$lib/TvRow.svelte';
+	import { uriArray} from '$store/movieRows'
+	  import Footer from '$lib/Footer.svelte'
+
+
+	const getGenres = async () => {
+		const res = await fetch('https://api.themoviedb.org/3/genre/tv/list?api_key=04c35731a5ee918f014970082a0088b1&language=en-US')
+		return await res.json();
+	}
+
+	const getMoviesByGenre = async () => {
+		const res = await fetch(`https://api.themoviedb.org/3/discover/tv?api_key=04c35731a5ee918f014970082a0088b1&with_genres=${id}`)
+		return await res.json();
+	}
+</script>
+
+<main class="pt-[5rem]">
+	{#await getGenres()}
+		loading...
+	{:then result}
+		{#each result.genres as {name,id}, index}
+			<TvRow name={`Trending ${name} Tv Shows`} uri={`https://api.themoviedb.org/3/discover/tv?api_key=04c35731a5ee918f014970082a0088b1&with_genres=${id}`} />
+		{/each}
+	{/await}
+		<Footer />
+
+</main>

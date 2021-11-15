54a628d2e5777b8b6a9361b612f8337789746681
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-15 21:28:50 +0530

    🎈new routes - film,tv,my list

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,15 @@
   import LogIn from '$routes/LogIn.svelte';
 
   import HomeNavBar from '$lib/HomeNavBar.svelte'
+  import Footer from '$lib/Footer.svelte'
 
   import Home from "$routes/main/Home.svelte";
   import Movies from '$routes/main/Movies.svelte'
   import TvShows from '$routes/main/TvShows.svelte'
   import Profile from '$routes/main/Profile.svelte'
+  import Film from '$routes/main/Film.svelte'
+  import Tv from '$routes/main/Tv.svelte'
+  import MyList from '$routes/main/MyList.svelte'
 
 </script>
 
@@ -34,5 +38,14 @@
 	</Route>
 	<Route path="/profile">
 		<Profile />
+	</Route>
+	<Route path="/my-list">
+		<MyList />
+	</Route>
+	<Route path="/film/:title" let:meta>
+		<Film />
+	</Route>
+	<Route path="/tv/:title" let:meta>
+		<Tv />
 	</Route>
 </Route>

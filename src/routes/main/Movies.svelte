5ea6a84c38aaf85add28b19c1e426d280a5d12f8
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-15 21:40:38 +0530

    🎈 MOVIE route - only show's details about movies categorized based on genres, trends

--- src/routes/main/Movies.svelte
+++ src/routes/main/Movies.svelte
@@ -1,1 +1,31 @@
-<p>Movies</p>
+<script>
+	import Row from '$lib/Row.svelte';
+	import { uriArray} from '$store/movieRows'
+	  import Footer from '$lib/Footer.svelte'
+
+
+	const getGenres = async () => {
+		const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=04c35731a5ee918f014970082a0088b1&language=en-US')
+		return await res.json();
+	}
+
+	const getMoviesByGenre = async () => {
+		const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&with_genres=${id}`)
+		return await res.json();
+	}
+</script>
+
+<main class="pt-[5rem]">
+	{#await getGenres()}
+		<p class="text-gray-100">loading...</p>
+	{:then result}
+		{#each result.genres as {name,id}, index}
+			<Row name={`Popular ${name} Movies`} uri={`https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&with_genres=${id}`} />
+		{/each}
+	{/await}
+	{#each $uriArray as {name,uri}, index}
+		<Row {name} {uri} />
+	{/each}
+		<Footer />
+
+</main>

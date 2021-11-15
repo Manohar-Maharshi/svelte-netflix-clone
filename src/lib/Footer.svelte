b6f1326f918d62c26210da7464404aaf475bab19
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-15 21:31:41 +0530

    🍟 classic footer 😎

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,10 @@
+<footer class="bg-[#090909] md:px-0 px-4 w-full text-right py-3">
+	<p class="container mx-auto text-sm flex items-center justify-end space-x-1">
+		<span>
+			Made with Svelte
+		</span>
+		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current text-red-500" viewBox="0 0 20 20" fill="currentColor">
+		  <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+		</svg>
+	</p>
+</footer>

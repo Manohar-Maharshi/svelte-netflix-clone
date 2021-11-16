e80d1a14655b164541098a3e79c37d92649e9413
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-16 21:01:43 +0530

    Update Error.svelte

--- src/lib/Error.svelte
+++ src/lib/Error.svelte
@@ -1,4 +1,4 @@
 <main class="w-full h-screen grid place-items-center">
-	<h1 class="text-[6rem] leading-none">Hoo , You are in 404 area</h1>
+	<h1 class="md:text-[6rem] leading-none">Hoo , You are in 404 area</h1>
 	<a href="/browse" class="text-blue-600 text-xl">Go Home</a>
 </main>
